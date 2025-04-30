b5b1bae4b842e9fa1f4faeda6ede813ffc0de3d6
MiguelCastilloSanchez <A18003592@alumnos.uady.mx>
2025-04-30 16:32:56 -0600

    main files

---
+++ src/main/java/com/example/fleetmasterauth/FleetmasterauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.fleetmasterauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetmasterauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetmasterauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fleetmasterauth/FleetmasterauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fleetmasterauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetmasterauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
