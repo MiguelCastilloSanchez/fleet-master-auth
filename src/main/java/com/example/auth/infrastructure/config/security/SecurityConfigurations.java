2f264ac1fe7b7aeca81781ea4cf294cf23af7a73
diegoalamilla <diego.alamilla123@gmail.com>
2025-05-29 17:24:10 -0600

    CORS GLOBAL With VM IP

--- src/main/java/com/example/auth/infrastructure/config/security/SecurityConfigurations.java
+++ src/main/java/com/example/auth/infrastructure/config/security/SecurityConfigurations.java
@@ -53,7 +53,7 @@ public class SecurityConfigurations {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://128.85.32.202"));
+        configuration.setAllowedOriginPatterns(List.of("*")); // o "*"
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
